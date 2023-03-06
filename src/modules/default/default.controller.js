8525eeedaeb8b176e585dd3233b8668edb657e39
Roger <rogerchao98@gmail.com>
2023-03-06 17:04:35 +0100

    Update default.controller.js

--- src/modules/default/default.controller.js
+++ src/modules/default/default.controller.js
@@ -6,7 +6,7 @@ class Controller {
       res.send('Hello World').end();
     } else {
 
-      res.json({text: 'Hello bro speaking straight from a GitHub commit. It is 14:33.'}).end();
+      res.json({text: 'Hello bro speaking straight from a GitHub commit. It is 17:04.'}).end();
     }
   }
 }
